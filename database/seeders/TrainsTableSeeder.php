09a6b1a96286f2c983832a8ceb9abb15fbb67b26
Christian01881 <chrispamo01881@gmail.com>
2023-05-29 01:03:22 +0200

    project finished

---
+++ database/seeders/TrainsTableSeeder.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Train;
+
+class TrainsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $cities = [
+            'Roma Centrale',
+            'Milano Centrale',
+            'Napoli Centrale',
+            'Ostia',
+            'Palermo',
+            'Parma',
+            'Taranto',
+            'Firenze',
+            'Genova'
+        ];
+
+        $agency = [
+            'Italo',
+            'Trenitalia'
+        ];
+
+        $faker = \Faker\Factory::create();
+        for ($i = 0; $i < 100; $i++) {
+            $fakedate = $faker->dateTimeBetween('-10 days', '+15 days')->format('Y-m-d H:i:s');
+            $randIndAgency = array_rand($agency);
+            $randIndCitiesA = array_rand($cities);
+            $randIndCitiesB = array_rand($cities);
+            while ($randIndCitiesA === $randIndCitiesB) {
+                $randIndCitiesB = array_rand($cities);
+            }
+            $train['agency'] = $agency[$randIndAgency];
+            $train['from_station'] = $cities[$randIndCitiesA];
+            $train['to_station'] = $cities[$randIndCitiesB];
+            $train['date_of_departure'] = $fakedate;
+            $train['date_of_arrival'] = $faker->dateTimeInInterval($fakedate,"+2 hours");
+            $train['on_time'] = rand(0,1);
+            $train['canceled'] = rand(0,1);
+            $trains[] = $train;
+            }
+
+
+
+
+        foreach ($trains as $train) {
+            $train['train_code'] = rand(6000, 9999);
+            $train['train_cars'] = rand(1, 9);
+            $newTrain = new Train();
+            $newTrain->agency = $train['agency'];
+            $newTrain->from_station = $train['from_station'];
+            $newTrain->to_station = $train['to_station'];
+            $newTrain->date_of_departure = $train['date_of_departure'];
+            $newTrain->date_of_arrival = $train['date_of_arrival'];
+            $newTrain->train_code = $train['train_code'];
+            $newTrain->train_cars = $train['train_cars'];
+            $newTrain->on_time = $train['on_time'];
+            $newTrain->canceled = $train['canceled'];
+            $newTrain->save();
+        }
+    }
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,20 @@
 @extends('layouts.general')
 
 @section('content')
-    <main>
-
+    <main class="d-flex flex-column">
+        @foreach ($trains as $train)
+        <div class="d-flex align-items-end">
+            <h5 class="pt-4">Agenzia: {{ $train->agency }}</h5>
+            <h5 class="pt-4 ps-4">Da: {{ $train->from_station }}</h5>
+            <h5 class="pt-4 ps-4">A: {{ $train->to_station }}</h5>
+            <h5 class="pt-4 ps-4">Treno N°: {{ $train->train_code }}</h5>
+            <h5 class="pt-4 ps-4">N° carrozze: {{ $train->train_cars }}</h5>
+            <h5 class="pt-4 ps-4">{{ $train->date_of_departure }}</h5>
+            <h5 class="pt-4 ps-4">{{ $train->date_of_arrival }}</h5>
+            @if ($train->canceled == 0)
+                <h5 class="ps-5 ms-5">Cancellato</h5>
+            @endif
+        </div>
+        @endforeach
     </main>
 @endsection
